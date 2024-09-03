84119cbe0c87ec70a6c99d01146be121c6a38bae
hitisha-goyani <goyanihitisha@gmail.com>
2024-09-03 17:16:59 +0530

    commit

---
+++ script.js
@@ -0,0 +1,40 @@
+$(".btn1").click(function(){
+    $("div").hide();
+
+})
+$(".btn2").click(function(){
+    $("div").show();
+
+})
+$(".btn3").click(function(){
+    $("div").toggle();
+
+})
+$(".btn4").click(function(){
+    $("div").slideToggle();
+
+})
+$(".btn5").click(function(){
+    $("div").slideUp();
+
+})
+$(".btn6").click(function(){
+    $("div").slideDown();
+
+})
+$(".btn7").click(function(){
+    $("div").fadeIn("2000");
+
+})
+$(".btn8").click(function(){
+    $("div").fadeOut("2000");
+
+})
+$(".btn9").click(function(){
+    $("div").fadeToggle("slow");
+
+})
+$(".btn10").click(function(){
+    $("div").fadeTo("slow",0.7);
+
+})
